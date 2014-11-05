58ccae18375187490392e2eff11dbe14d5f8a332
Brian Vermillion <BMVermillion@gmail.com>
2014-11-05 09:47:08 -0500

    Get and set methods for Game objects

--- src/objects/GameObject.java
+++ src/objects/GameObject.java
@@ -1,5 +1,36 @@
 package objects;
 
-public class GameObject {
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
 
+public class GameObject {
+	private int ID;
+	private float[] location;
+	private Dimension size;
+	private BufferedImage texture;
+	
+	
+	public void setDimension(Dimension D) {
+		size = D;
+	}
+	
+	public Dimension getDimension() {
+		return size;
+	}
+	
+	public void setLocation(float[] L) {
+		location = L;
+	}
+	
+	public float[] getLocation() {
+		return location;
+	}
+	
+	public BufferedImage getTexture() {
+		return texture;
+	}
+	
+	public void setTexture(BufferedImage I) {
+		texture = I;
+	}
 }
