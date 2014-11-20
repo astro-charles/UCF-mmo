080fcc6e0b2c98c0955fdbbb1a2ab20dd4f06afb
char-tay <charlieataylor@gmail.com>
2014-11-19 21:47:22 -0500

    added tylers server changes

--- src/server/ClientCommunicator.java
+++ src/server/ClientCommunicator.java
@@ -1,5 +1,54 @@
 package server;
 
-public class ClientCommunicator {
+import java.net.Socket;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
+public class ClientCommunicator{
+	private Socket socket;
+	private boolean connected;
+	private Handler handler;
+
+	private class Handler extends Thread{
+		private ObjectInputStream in;
+		
+                public void run(){
+			try{
+				in = new ObjectInputStream(socket.getInputStream());
+			}
+			catch(Exception e){
+				System.out.println(e);
+				return;
+			}
+		}
+	}
+
+	public ClientCommunicator(Socket newSocket){
+
+		socket = newSocket;
+		connected = true;
+
+		handler = new Handler();
+		handler.start();
+	}
+
+	public boolean isConnected(){
+		return connected;
+	}
+
+	public void purge(){
+		// Close everything
+		try{
+			connected = false;
+			socket.close();
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+	}
+
+	public String toString(){
+		return new String(socket.toString());
+	}
 }
+
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,5 +1,40 @@
 package server;
 
-public class Server {
+import java.net.*;
+import java.io.*;
+import java.util.*;
 
+public class Server extends Thread{
+        private ServerSocket server;
+	private Socket socket;
+	private ArrayList<ClientCommunicator> clients = new ArrayList<ClientCommunicator>();
+
+	public Server(){
+		try{
+			server = new ServerSocket();
+		}
+		catch(IOException e){System.out.println(e);}
+	}
+        
+        @Override
+	public void run(){
+		while(true){
+			// Remove all disconnected clients
+			for(int i = 0;i < clients.size();i++){
+				// Check connection, remove on dead
+				if(!clients.get(i).isConnected()){
+					System.out.println(clients.get(i));
+					clients.remove(i);
+				}
+			}
+			try{
+				socket = server.accept();
+			}
+			catch(Exception e){System.out.println(e);}
+
+			clients.add(new ClientCommunicator(socket));
+		}
+	}
 }
+
+
