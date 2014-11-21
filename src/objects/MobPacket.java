de32d8453a7ca3af6301c2fb4f95635f5f10f5b4
tlogsdon <tlogsdon0@gmail.com>
2014-11-21 01:59:42 -0500

    commit of server things and stuff

---
+++ src/objects/MobPacket.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objects;
+
+/**
+ *
+ * @author Elucid
+ */
+public class MobPacket {
+    public String name;
+    public int posx;
+    public int posy;
+    public boolean kill;
+}
